6479a379bc44d09684ceb378348dcacaca3ba6a2
Kolton Underberg <Kolton.Underberg@vu.com>
2023-04-21 14:27:54 -0500

    for loop with two different lists

--- displayInventory.py
+++ displayInventory.py
@@ -1,7 +1,15 @@
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
 def displayInventory(inventory):
-    # your code goes here
+    total_items = 0
+    print("Inventory:")
+
+    for item, count in inventory.items():
+        print(count, item)
+        total_items += count
+    
+    print(f"total number of items: {total_items}")
+    
 
 
 if __name__ == "__main__":
